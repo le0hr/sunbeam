918fe198f2d368e74431d25c20aed31fa81f1160
le0hr <lev.hrabovij@gmail.com>
2026-07-18 16:16:36 +0300

    routes fixed

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: "http://localhost/api",
+  baseURL: "https://sunbeambe.com/api",
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
