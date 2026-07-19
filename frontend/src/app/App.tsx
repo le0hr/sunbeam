ca6e8385efc69eb8bb3f6b7734053ddbadee382e
le0hr <lev.hrabovij@gmail.com>
2026-07-19 21:07:40 +0300

    google ads tags

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,6 +1,15 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
+import { useLocation } from "react-router-dom";
+import ReactGA from "react-ga4"
+import { useEffect } from "react";
 
 export default function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
   return <RouterProvider router={router} />;
 }
--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -1,7 +1,7 @@
 
   import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
-  import "../styles/index.css";
+  import ReactGA from "react-ga4";
 
+  ReactGA.initialize("AW-18334396152");
   createRoot(document.getElementById("root")!).render(<App />);
-  
