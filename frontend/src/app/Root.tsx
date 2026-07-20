0de6b4341938e59a2c2e9f6d68622a84b5196e45
le0hr <lev.hrabovij@gmail.com>
2026-07-20 10:17:44 +0300

    frontend fixes

--- frontend/src/app/Root.tsx
+++ frontend/src/app/Root.tsx
@@ -1,10 +1,31 @@
 import { Outlet } from 'react-router';
 import {Header} from '../components/common/Header'; 
 import {Footer} from '../components/common/Footer';
+import { useLocation } from "react-router-dom";
+import ReactGA from "react-ga4"
+import { useEffect } from "react";
+
+
+
+function Analytics() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname + location.search,
+    });
+  }, [location]);
+
+  return null;
+}
+
+
 
 const Root = () => {
   return (
     <div className="app-container">
+      <Analytics />
       {/* Хедер рендериться один раз і буде всюди */}
       <Header /> 
       
